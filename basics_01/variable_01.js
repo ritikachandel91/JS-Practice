46da04c5add19c5d72b35ffe98b9f44f37a118a5
Ritika Ashok Chandel <115643405+ritikachandel91@users.noreply.github.com>
2026-09-15 06:49:57 +0000

    container config done

---
+++ basics_01/variable_01.js
@@ -0,0 +1,19 @@
+const accountId =  144553
+let accountEmail = "ritika@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 // reasign not allowed bcz it's const
+accountEmail = "ritz@google.com"
+accountPassword = "909090"
+accountCity = "Kerela"
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+console.log(accountState)
