46df843be53ce9f9ae2ad0031c65a2c5a8f83bb6
Ritika Ashok Chandel <115643405+ritikachandel91@users.noreply.github.com>
2026-09-15 08:26:24 +0000

    container config done

---
+++ basics_01/datatype_01.js
@@ -0,0 +1,29 @@
+"use strict" // treat all js code as newer version
+
+//alert("hello") // we are using nodejs , not browser
+
+let name = "Ritika"
+let age = 27
+let b = false
+//number = 2 to power 53 memory address
+//bigint for big number
+//String
+//boolean = true /false
+// null=standalone value
+//undefine
+//symbol
+//object
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof b)
+
+let num = 10
+if(num%2==0){
+    console.log("even")
+}else{
+    console.log("odd")
+}
+
+console.log(typeof null)
+console.log(typeof undefined)
