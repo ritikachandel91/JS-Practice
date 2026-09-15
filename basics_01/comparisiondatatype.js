f204e1bc152c5bdb59d52f9a6fd029a75387b90e
Ritika Ashok Chandel <115643405+ritikachandel91@users.noreply.github.com>
2026-09-15 11:36:05 +0000

    container config done

---
+++ basics_01/comparisiondatatype.js
@@ -0,0 +1,10 @@
+console.log(2>1)//true
+
+console.log("2">1)//true
+console.log(null>0)//f
+console.log(null==0)//f
+console.log(null>=0)//t
+console.log(undefined<0)//f
+console.log(undefined==0)//f
+
+console.log("2"===2);
---
+++ basics_01/operation.js
@@ -0,0 +1,23 @@
+let value =3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Ritika"
+let str3 = str1+str2
+console.log(str3)
+
+console.log(1+2+"2")
+console.log("1"+2+2)
+console.log((3+4)*5%3);
+
+console.log(+true)
+
+let num1 ,num2, num3
+num1 = num2=num3 = 2+2
+let gameCounter=20;
+gameCounter++;
+gameCounter;
+console.log(++gameCounter);
+gameCounter--;
+console.log(++gameCounter);
