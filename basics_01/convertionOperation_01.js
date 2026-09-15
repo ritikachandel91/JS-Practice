c5a6a92e830922ac1c5e924e0a7eb4467c7812a0
Ritika Ashok Chandel <115643405+ritikachandel91@users.noreply.github.com>
2026-09-15 09:34:45 +0000

    container config done

---
+++ basics_01/convertionOperation_01.js
@@ -0,0 +1,24 @@
+let score ="23a"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" == 33
+//"33abc" == nan
+//tru == 1 , false ==0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 == true ; 0=false
+//" == false
+// //hitesh "==true
+
+let someNumber = 33
+let stringnumber = String(someNumber)
+console.log(typeof stringnumber)
