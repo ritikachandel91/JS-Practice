677f88306c0999dbfa8a2fffc3ce3e17593f6330
Ritika Ashok Chandel <115643405+ritikachandel91@users.noreply.github.com>
2026-09-16 10:16:00 +0000

    container config done

--- basics_01/datatype_01.js
+++ basics_01/datatype_01.js
@@ -27,3 +27,45 @@ if(num%2==0){
 
 console.log(typeof null)
 console.log(typeof undefined)
+
+//Primitive -->
+//7types :String , number , boolean , null , undefined , symbol , bigInt 
+
+let s = "Ritika"
+console.log(typeof s);
+
+let w = 10
+console.log(typeof w);
+
+let e = "Ritika"
+console.log(typeof e);
+
+let t = 10>2
+console.log(typeof t);
+
+let n =null
+console.log(typeof n);
+
+let u = undefined
+console.log(typeof u);
+
+let id = Symbol('123')
+console.log(typeof id)
+
+const p = Symbol('123')
+console.log(id === p)
+//Refereence (Non primitive)
+
+//Arrays , objects , Functions
+const hero =["shaktiman","naagraj","doga"];
+let myobj = {
+    name :"ritika",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
+console.log(hero)
