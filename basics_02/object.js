0d37596b836fc7e80e11c2d79ebd3216134709d7
Ritika Ashok Chandel <115643405+ritikachandel91@users.noreply.github.com>
2026-09-18 10:19:15 +0000

    container config done

--- basics_02/object.js
+++ basics_02/object.js
@@ -23,3 +23,35 @@ Object.freeze(Jsuser)
 Jsuser.name = "Ram"
 console.log(Jsuser.email)
 console.log(Jsuser.name)
+
+//console.log(Object.values(tinderUser))
+//console.log(Object.entries(tinderUser))
+
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+const  course = {
+    coursename : "js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+const {courseInstructor} = course
+console.log(courseInstructor)
+
+const user = {
+    name: "Ritika",
+    age: 27,
+    city: "Pune"
+};
+
+const { name, age } = user;
+
+console.log(name); // Ritika
+console.log(age);  // 27
+
+//JOSN
+
+{
+    name:"Ritika-----"
+    coursename:"js in English"
+    price:"free"
+}
